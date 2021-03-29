feb4acee039af9e2cc39f69d0d99f96479b9edb3
Carlos Arango <cmarango99@gmail.com>
2021-03-29 15:25:01 -0600

    main2 y agregando cosas a la ventana

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+//Clase restaurante
+// Se supone que debe controlar las demás clases aparte de manejar
+//Listas de clientes, productos random y lista de produccion
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
 	// pass
 
     }
---
+++ src/Ventana_Main.java
@@ -0,0 +1,39 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.plaf.DimensionUIResource;
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+public class Ventana_Main extends JFrame implements ActionListener {
+
+    JLabel imagen;
+    JLabel etiquetaImagen;
+
+    JButton boton1;
+    JButton boton2;
+
+    JPanel panelImagen;
+    JPanel panelBotones;
+
+    ImageIcon icon1;
+    ImageIcon icon2;
+
+    public Ventana_Main() {
+        this.setPreferredSize(new DimensionUIResource(500, 500)); //Resolucion
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.pack();
+        this.setVisible(true);
+    }
+    
+    private void configuraciones(){
+        panelImagen = new JPanel();
+        panelBotones = new JPanel();
+
+        //imagen_fondo = new ImageIcon()
+    }
+}
